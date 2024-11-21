378dbcafd0e0d7875ce32688b9d2b8d39e543cc9
TZR71 <Tiinarojola@gmail.com>
2024-11-21 06:24:09 +0100

    took noopener's away as Netlify implied so

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ export default function App() {
         <Weather defaultCity="Palermo" />
         <footer>
           This project was created by{" "}
-          <a href="https://github.com/TZR71" target="_blank" rel="<noopener noreferrer>">
+          <a href="https://github.com/TZR71" target="_blank" rel="<noreferrer>">
             Tiina Rojola{" "}
           </a>{" "}
           and is open-sourced on {""}
           <a
             href="https://github.com/TZR71/weather-react"
             target="_blank"
-            rel="<noopener noreferrer>"
+            rel="<noreferrer>"
           >
             GitHub
           </a>{" "}
@@ -24,7 +24,7 @@ export default function App() {
           <a
             href="https://tr-weather-react.netlify.app/"
             target="_blank"
-            rel="<noopener noreferrer>"
+            rel="<noreferrer>"
           >
             Netlify
           </a>
